30c09e93ad3caa01ca6607b991cb17b9637dd1de
X <androysol@gmail.com>
2023-04-06 16:47:15 +0300

    feat(NavBar UI):
    NavBar UI created, animation for nav items, social links and logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
 import '@/styles/globals.css'
+import {Montserrat} from 'next/font/google'
+import Head from 'next/head'
+import NavBar from '@/components/NavBar'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const montserrat = Montserrat({
+	subsets: ['latin'],
+	variable: '--font-mont'
+})
+
+export default function App({Component, pageProps}) {
+	return (
+		<>
+			<Head>
+				{' '}
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+			</Head>
+			<main
+				className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+			>
+				<NavBar />
+				<Component {...pageProps} />
+			</main>
+		</>
+	)
 }
